85f30f3278519c6f0aedca1dcb97ca8ac9429604
Raymen Deol <deol.raminder@gmail.com>
2020-11-13 12:08:09 -0800

    Add files via upload

--- starwars_app/src/components/Planets/Planets.js
+++ starwars_app/src/components/Planets/Planets.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import FormattedTable from '../FormattedTable/FormattedTable.js'
 
+//Fetch API inside "main" Planets component
+// Pass relevant API response info in Array to <FormattedTable /> component 
+
 const Planets = () => {
 
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [planets, setPlanets] = useState([]);
     const filteredPlanetData = [];
+
+    //Filters API response for 'name', 'terrain' and 'population'
+    function filterPlanetData(planet, index, arr) {
+        filteredPlanetData.push(arr[index].name, arr[index].terrain, arr[index].population);
+    };
 
     useEffect(() => {
      fetch('https://swapi.dev/api/planets/')   
@@ -25,24 +33,18 @@ const Planets = () => {
         }, [])
 
     planets.forEach(filterPlanetData);
-    
-    function filterPlanetData(planet, index, arr) {
-
-        filteredPlanetData.push(arr[index].name, arr[index].terrain, arr[index].population);
-
-    };
-
-   
 
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
         return <div>Loading...</div>;
     } else {
+        //Pass filteredPlanetData as prop to the Formatted Table Component
+        // FormattTable will return JSX for formatted material UI table
         return (
             <div>
             <h2>Planets in the Starwars Galaxy:</h2>
-            {<FormattedTable planets = {filteredPlanetData}/>}
+            {<FormattedTable planets = {filteredPlanetData}/>} 
         </div>     
         );
     }
